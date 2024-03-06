725cb1db8f843a9200ab672a1f88937d917fedf2
aliqyan-21 <aliqyanabid21@gmail.com>
2024-03-06 19:25:10 +0530

    some improvements and use of lambda functions

--- main.cpp
+++ main.cpp
@@ -1,117 +1,153 @@
 #include <iostream>
 #include <random>
-using namespace std;
+
+auto easy = []() {
+  std::random_device rd;
+  std::uniform_int_distribution<int> gen(1, 100);
+
+  int randNum = gen(rd);
+  int userGuess;
+  int count = 1;
+
+  std::cout << "Guess the number I am thinking of between 1 and 100..."
+            << std::endl;
+  std::cout << "Guess: ";
+  std::cin >> userGuess;
+
+  while (true) {
+    if (userGuess > randNum) {
+      count++;
+
+      std::cout << "I am thinking of a smaller number than that..."
+                << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else if (userGuess < randNum) {
+      count++;
+
+      std::cout << "I am thinking of a larger number than that..." << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else {
+      std::cout << "Yes, you guessd it right " << userGuess
+                << " is the number I was thinking of!" << std::endl;
+      std::cout << "Total number of tries you took: " << count << std::endl;
+      break;
+    }
+  }
+};
+
+auto medium = []() {
+  std::random_device rd;
+  std::uniform_int_distribution<int> gen(1, 200);
+
+  int randNum = gen(rd);
+  int userGuess;
+  int count = 0;
+
+  std::cout << "Guess the number I am thinking of between 1 and 200..."
+            << std::endl;
+  std::cout << "Guess: ";
+  std::cin >> userGuess;
+
+  while (true) {
+    if (userGuess > randNum) {
+      count++;
+
+      std::cout << "I am thinking of a smaller number than that..."
+                << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else if (userGuess < randNum) {
+      count++;
+
+      std::cout << "I am thinking of a larger number than that..." << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else {
+      std::cout << "Yes, you guessd it right " << userGuess
+                << " is the number I was thinking of!" << std::endl;
+      std::cout << "Total number of tries you took: " << count << std::endl;
+      break;
+    }
+  }
+};
+
+auto hard = []() {
+  std::random_device rd;
+  std::uniform_int_distribution<int> gen(1, 300);
+
+  int randNum = gen(rd);
+  int userGuess;
+  int count = 0;
+
+  std::cout << "Guess the number I am thinking of between 1 and 300..."
+            << std::endl;
+  std::cout << "Guess: ";
+  std::cin >> userGuess;
+
+  while (true) {
+    if (userGuess > randNum) {
+      count++;
+
+      std::cout << "I am thinking of a smaller number than that..."
+                << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else if (userGuess < randNum) {
+      count++;
+
+      std::cout << "I am thinking of a larger number than that..." << std::endl;
+      std::cout << "Try again: ";
+      std::cin >> userGuess;
+    } else {
+      std::cout << "Yes, you guessd it right " << userGuess
+                << " is the number I was thinking of!" << std::endl;
+      std::cout << "Total number of tries you took: " << count << std::endl;
+      break;
+    }
+  }
+};
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    cout << "Usage: ./main mode" << endl;
-    cout << "Mode = easy, medium, hard" << endl;
+    std::cout << "Usage: ./main mode" << std::endl;
+    std::cout << "Mode = easy, medium, hard" << std::endl;
     exit(1);
   }
 
-  string mode = argv[1];
+  std::string mode = argv[1];
+  char playagain;
 
-  if (mode == "easy") {
-    random_device rd;
-    uniform_int_distribution<int> gen(1, 100);
-
-    int randNum = gen(rd);
-    int userGuess;
-    int count = 0;
-
-    cout << "Guess the number I am thinking of between 1 and 100..." << endl;
-    cout << "Guess: ";
-    cin >> userGuess;
-
-    while (true) {
-      if (userGuess > randNum) {
-        count++;
-
-        cout << "I am thinking of a smaller number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else if (userGuess < randNum) {
-        count++;
-
-        cout << "I am thinking of a larger number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else {
-        cout << "Yes, you guessd it right " << userGuess
-             << " is the number I was thinking of!" << endl;
-        cout << "Total number of tries you took: " << count << endl;
-        cout << "Thanks for playing Good Bye..." << endl;
-        break;
-      }
+  while (true) {
+    if (mode == "easy") {
+      easy();
+    } else if (mode == "medium") {
+      medium();
+    } else if (mode == "hard") {
+      hard();
+    } else {
+      std::cout << "Not a valid mode input, modes = 'easy, medium, hard'"
+                << std::endl;
     }
-  } else if (mode == "medium") {
-    random_device rd;
-    uniform_int_distribution<int> gen(1, 200);
-
-    int randNum = gen(rd);
-    int userGuess;
-    int count = 0;
-
-    cout << "Guess the number I am thinking of between 1 and 200..." << endl;
-    cout << "Guess: ";
-    cin >> userGuess;
-
     while (true) {
-      if (userGuess > randNum) {
-        count++;
-
-        cout << "I am thinking of a smaller number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else if (userGuess < randNum) {
-        count++;
-
-        cout << "I am thinking of a larger number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else {
-        cout << "Yes, you guessd it right " << userGuess
-             << " is the number I was thinking of!" << endl;
-        cout << "Total number of tries you took: " << count << endl;
-        cout << "Thanks for playing Good Bye..." << endl;
+      std::cout << "Do you want to play again[y/n]: ";
+      std::cin >> playagain;
+      if (playagain == 'y' || playagain == 'n')
         break;
-      }
     }
-  } else if (mode == "hard") {
-    random_device rd;
-    uniform_int_distribution<int> gen(1, 300);
-
-    int randNum = gen(rd);
-    int userGuess;
-    int count = 0;
-
-    cout << "Guess the number I am thinking of between 1 and 300..." << endl;
-    cout << "Guess: ";
-    cin >> userGuess;
-
-    while (true) {
-      if (userGuess > randNum) {
-        count++;
-
-        cout << "I am thinking of a smaller number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else if (userGuess < randNum) {
-        count++;
-
-        cout << "I am thinking of a larger number than that..." << endl;
-        cout << "Try again: ";
-        cin >> userGuess;
-      } else {
-        cout << "Yes, you guessd it right " << userGuess
-             << " is the number I was thinking of!" << endl;
-        cout << "Total number of tries you took: " << count << endl;
-        cout << "Thanks for playing Good Bye..." << endl;
-        break;
+    if (playagain == 'n')
+      break;
+    else {
+      while (true) {
+        std::cout << "Which difficulty[easy, medium, hard]: ";
+        std::cin >> mode;
+        if (mode == "easy" || mode == "medium" || mode == "hard") {
+          break;
+        }
       }
+      continue;
     }
-  } else {
-    cout << "Not a valid mode input, modes = 'easy, medium, hard'" << endl;
   }
 
   return 0;
